7b6a6b9dbbc904a9e90ab2f5a51523e5d45de038
marcel1999-prog <evilchezbustamante@gmail.com>
2024-10-11 22:45:19 -0500

    programacion

--- importacion de base de datos.R
+++ importacion de base de datos.R
@@ -39,3 +39,91 @@ url <- "https://docs.google.com/spreadsheets/d/15r7ZwcZZHbEgltlF6gSFvCTFA-CFzVBW
 gs <- as_sheets_id(url)
 fb <- googlesheets4::range_read(ss=gs, sheet= "fb")
 
+library(agricolae)
+
+
+
+# DCA ---------------------------------------------------------------------
+
+
+
+# Definir los niveles de fertilización nitrogenada y el número de repeticiones
+fertilizacion <- c(0, 50, 100)  # Dosis de fertilización (kg/ha)
+repeticiones <- 5  # Número de repeticiones
+
+# Crear el diseño completamente al azar (DCA)
+# Para 15 unidades experimentales (3 tratamientos x 5 repeticiones)
+tratamientos <- rep(fertilizacion, each = repeticiones)
+unidades_experimentales <- 1:length(tratamientos)  # Identificar las unidades experimentales
+
+# Crear un marco de datos con los tratamientos y las unidades experimentales
+dca <- data.frame(Unidad = unidades_experimentales, Fertilizacion = tratamientos)
+
+# Mostrar el diseño experimental
+print(dca)
+# Randomizar el orden de los tratamientos
+set.seed(123)  # Fijar la semilla para la reproducibilidad
+dca$Fertilizacion <- sample(dca$Fertilizacion)
+
+# Mostrar el diseño experimental randomizado
+print(dca)
+
+
+# DBCA --------------------------------------------------------------------
+# fertilizante: 0, 50, 100
+# cultivar: canchan y peruanita
+trt<-c(3, 2) # factorial 3x2
+
+
+outdesign <-design.ab(trt, r=4, design = "rcbd")
+book<-outdesign$book
+
+book %>% str()
+
+ds <- book %>% 
+  mutate(ferti = case_when(
+    A %in% 1 ~ "0"
+    ,  A %in% 2 ~ "50"
+    ,  A %in% 3 ~ "100"
+  )) %>% 
+  mutate(cultivar = case_when(
+    B %in% 1 ~ "canchan"
+    ,  B %in% 2 ~ "peruanita"
+  )) 
+
+
+ds %>% 
+  openxlsx::write.xlsx(file = "dbca.xlsx")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
